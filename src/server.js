2188812e529e81a607798e49d79a8c66cf859155
Cmmh1101 <56605042+Cmmh1101@users.noreply.github.com>
2021-09-12 08:47:26 -0500

    adding post

--- src/server.js
+++ src/server.js
@@ -3,6 +3,18 @@ import bodyParser from "body-parser";
 
 const app = express();
 
+const articlesInfo = {
+  "learn-react": {
+    upvotes: 0,
+  },
+  "learn-node": {
+    upvotes: 0,
+  },
+  "my-thoughts-on-resumes": {
+    upvotes: 0,
+  },
+};
+
 // app.use(bodyParser); //deprecated method use the ones bellow
 // app.use(bodyParser.urlencoded());
 app.use(express.json());
@@ -13,12 +25,23 @@ app.use(
 );
 
 // get route
-app.get("/hello", (req, res) => res.send("hello!"));
+// app.get("/hello", (req, res) => res.send("hello!"));
 
 // get by name
-app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
+// app.get("/hello/:name", (req, res) => res.send(`Hello ${req.params.name}`));
 
 // create post route
-app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
+// app.post("/hello", (req, res) => res.send(`Hello ${req.body.name}!`));
+
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const articleName = req.params.name;
+
+  articlesInfo[articleName].upvotes + -1;
+  res
+    .status(200)
+    .send(
+      `${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`
+    );
+});
 
 app.listen(8000, () => console.log("listening on port 8000"));
